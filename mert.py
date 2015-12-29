7625803b5480fea9caa6d6f71145573193431381
kutay2783 <kutay2783@hotmail.com>
2015-12-29 17:20:39 +0200

    Update mert.py

--- mert.py
+++ mert.py
@@ -2,9 +2,9 @@ import Adafruit_BBIO.PWM as PWM
 pwmC="P8_13"
 pwmS=13
 PWM.start(pwmC,pwmS,50,0)
-while(1)
+while(1):
 	pwmS=input('Enter PWM: ')
-	if pwmS="q":
+	if pwmS=="q":
 		break
 	else:
 		PWM.set_duty_cycle(pwmC,pwmS)
